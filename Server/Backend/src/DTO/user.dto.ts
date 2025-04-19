b13f3b97f0cd8e86c430b3a74077f21a5b6b8979
William Fernandes <wiilfern1910@outlook.com>
2025-04-19 15:36:49 -0300

    Rascunho do Readme e FIX Zod Validation

--- Server/Backend/src/DTO/user.dto.ts
+++ Server/Backend/src/DTO/user.dto.ts
@@ -1,13 +1,9 @@
 import { z } from 'zod';
 
-export type UserDTO = {
-  name: string;
-  email: string;
-  password: string;
-};
-
 export const ZodValidateUser = z.object({
   name: z.string().min(6).max(12),
   email: z.string().email(),
   password: z.string().min(6).max(12),
 });
+
+export type UserDTO = z.infer<typeof ZodValidateUser>;
