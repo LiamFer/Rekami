4e1a4e69f12a65cc25452beb8f2624517d9ab0d9
William Fernandes <wiilfern1910@outlook.com>
2025-04-21 15:51:49 -0300

    Testando o Multer com a Profile Picture

--- Server/Backend/src/Modules/User/user.controller.ts
+++ Server/Backend/src/Modules/User/user.controller.ts
@@ -1,14 +1,30 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Req,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from 'src/guards/jwt-auth/jwt-auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @UseGuards(JwtAuthGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get("profile")
+  @Get('profile')
   getProfile(@Req() req) {
-    return req.user
+    return req.user;
+  }
+
+  @Post('upload/photo')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadPhoto(@UploadedFile() file: Express.Multer.File, @Req() req) {
+    console.log(file);
+    return req.user;
   }
 }
