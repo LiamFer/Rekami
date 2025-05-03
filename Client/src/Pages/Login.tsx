00890df1e5384ce1b3a098b58303cf21e51f75de
William Fernandes <wiilfern1910@outlook.com>
2025-05-03 12:41:49 -0300

    feat: Login Page start

--- Client/src/Pages/Login.tsx
+++ Client/src/Pages/Login.tsx
@@ -1,5 +1,30 @@
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "start",
+        gap: "20px",
+        padding: "20px",
+        minHeight: "100vh",
+        height: "100%",
+        boxSizing: "border-box",
+      }}
+    >
+      <div style={{flexGrow:1, backgroundColor: "red", height: "100%" }}>
+        <h1>login</h1>
+      </div>
+      <img
+        style={{
+          width:"100%",
+          height: "100%",
+          borderRadius: 40,
+          objectFit: "cover",
+        }}
+        src="https://wallpapercave.com/wp/wp6058611.jpg"
+        alt=""
+      />
+    </div>
+  );
 }
