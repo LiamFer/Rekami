c2eb7b32e6c908d89d866d1898842085f3b9e189
William Fernandes <wiilfern1910@outlook.com>
2025-05-09 11:51:38 -0300

    feat: Related Card Responsivo

--- Client/src/Components/MediaOtherOptions/MediaRelated.tsx
+++ Client/src/Components/MediaOtherOptions/MediaRelated.tsx
@@ -1,17 +1,21 @@
+import { Row, Col } from "antd";
 import { FullAnime } from "../../Types/FullAnime";
 import RelationCard from "../RelationCard/RelationCard";
 
 export default function MediaRelated({ animeFull }: { animeFull: FullAnime }) {
   return (
-    <div>
+    <div style={{display:"flex",flexDirection:"column",gap:"10px"}}>
       {animeFull.relations.map((r) => (
         <div>
           {r.relation}
-          <div>
+
+          <Row gutter={[16, 16]}>
             {r.entry.map((e) => (
-              <RelationCard relation={e}></RelationCard>
+              <Col key={e.mal_id} xs={24} sm={12} md={8}>
+                <RelationCard relation={e}></RelationCard>
+              </Col>
             ))}
-          </div>
+          </Row>
         </div>
       ))}
     </div>
--- Client/src/Components/RelationCard/RelationCard.tsx
+++ Client/src/Components/RelationCard/RelationCard.tsx
@@ -2,7 +2,11 @@ import { Card, Tag, Typography } from "antd";
 import { AnimeRelation } from "../../Types/AnimeRelation";
 const { Text } = Typography;
 
-export default function RelationCard({ relation }: { relation: AnimeRelation }) {
+export default function RelationCard({
+  relation,
+}: {
+  relation: AnimeRelation;
+}) {
   return (
     <Card
       style={{ flexGrow: 1, overflow: "hidden" }}
@@ -10,9 +14,16 @@ export default function RelationCard({ relation }: { relation: AnimeRelation }) 
     >
       <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <Tag style={{ borderRadius: "10px", width: "fit-content" }}>
-            {relation.type}
-          </Tag>
+          <h3
+            style={{
+              margin: 0,
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+            }}
+          >
+            {relation.name}
+          </h3>
         </div>
         <h3
           style={{
@@ -22,9 +33,13 @@ export default function RelationCard({ relation }: { relation: AnimeRelation }) 
             whiteSpace: "nowrap",
           }}
         >
-          {relation.name}
+          <Tag
+            color="orange"
+            style={{ borderRadius: "10px", width: "fit-content" }}
+          >
+            {relation.type}
+          </Tag>
         </h3>
-        
       </div>
     </Card>
   );
