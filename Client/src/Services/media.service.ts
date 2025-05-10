8a5a537fd63e847eea24885ccf9e014ff82dcd8f
William Fernandes <wiilfern1910@outlook.com>
2025-05-10 13:34:17 -0300

    feat: Save Media no MongoDB

--- Client/src/Services/media.service.ts
+++ Client/src/Services/media.service.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { serverApi } from "../Api/server";
 import { interestValue } from "../Types/interestValue";
 import { mediaType } from "../Types/mediaType";
+import { getAnimeFull } from "./jikan.service";
 
 export async function saveInterest(
   value: interestValue,
@@ -9,10 +10,29 @@ export async function saveInterest(
   mediaType: mediaType
 ) {
   try {
+    await saveMediaMongo(mediaId!)
     const response = await serverApi.post(`/interest/add`, {
       mediaId,
       value,
       mediaType,
+    });
+    return { success: true, data: response.data };
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      const message = error.response?.data?.message || "Unknown Error";
+      return { success: false, error: message };
+    }
+
+    return { success: false, error: "Unexpected Error." };
+  }
+}
+
+export async function saveMediaMongo(mediaId: string | number,
+) {
+  try {
+    const media = await getAnimeFull(mediaId) 
+    const response = await serverApi.post(`/media/add`, {
+      data: media,
     });
     return { success: true, data: response.data };
   } catch (error: any) {
