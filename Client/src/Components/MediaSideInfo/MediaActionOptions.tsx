e8b3001e8f1c95aee6985daedf9e74ca3c92b01b
William Fernandes <wiilfern1910@outlook.com>
2025-05-12 19:25:05 -0300

    Teste nos Buttons

--- Client/src/Components/MediaSideInfo/MediaActionOptions.tsx
+++ Client/src/Components/MediaSideInfo/MediaActionOptions.tsx
@@ -1,21 +1,38 @@
-import { ShareAltOutlined, DislikeOutlined, LikeOutlined, BookOutlined } from '@ant-design/icons'
-import { Button, theme } from 'antd'
+import {
+  ShareAltOutlined,
+  DislikeOutlined,
+  LikeOutlined,
+  BookOutlined,
+} from "@ant-design/icons";
+import { Button, theme } from "antd";
 
 export default function MediaActionOptions() {
   const { token } = theme.useToken();
   return (
     <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            width: "100%",
-            backgroundColor: token.colorBgContainerDisabled,
-          }}
-        >
-          <Button type="text" icon={<ShareAltOutlined />} style={{ flex: 1 }} />
-          <Button type="text" icon={<DislikeOutlined />} style={{ flex: 1 }} />
-          <Button type="text" icon={<LikeOutlined />} style={{ flex: 1 }} />
-          <Button type="text" icon={<BookOutlined />} style={{ flex: 1 }} />
-        </div>
-  )
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        width: "100%",
+        backgroundColor: token.colorBgContainerDisabled,
+      }}
+    >
+      <Button type="text" icon={<ShareAltOutlined />} style={{ flex: 1 }} />
+      <Button
+        variant="solid"
+        type="text"
+        color="red"
+        icon={<DislikeOutlined />}
+        style={{ flex: 1 }}
+      />
+      <Button
+        variant="solid"
+        type="text"
+        color="blue"
+        icon={<LikeOutlined />}
+        style={{ flex: 1 }}
+      />
+      <Button type="text" icon={<BookOutlined />} style={{ flex: 1 }} />
+    </div>
+  );
 }
