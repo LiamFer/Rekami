f47be37c0f735a7c78450a2f61cd63ef24bc883e
William Fernandes <wiilfern1910@outlook.com>
2025-05-21 11:24:05 -0300

    feat: Media Status Button

---
+++ Client/src/Types/mediaStatus.ts
@@ -0,0 +1,9 @@
+enum MediaStatus {
+    Watched = "watched",     
+    Watching = "watching",   
+    ToWatch = "to_watch",    
+    Paused = "paused",      
+    Planning = "planning",   
+}
+
+export default MediaStatus;
