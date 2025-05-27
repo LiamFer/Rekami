929b357f70c7c8b06f7cb96dc06ceebb606f1da2
William Fernandes <wiilfern1910@outlook.com>
2025-05-27 17:59:20 -0300

    feat: Buscando as Medias do Usuário

--- Client/src/Pages/Library.tsx
+++ Client/src/Pages/Library.tsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
 import Loading from "../Components/Loading/Loading";
+import { getLibrary } from "../Services/media.service";
 
 export default function Library() {
   const [library, setLibrary] = useState([]);
   const [loading, setLoading] = useState(true);
-
-
+  useEffect(() => {
+    const fetchLibrary = async () => {
+      await getLibrary().then((res) => {
+        setLibrary(res.data);
+        setLoading(false);
+      });
+    };
+    fetchLibrary();
+  }, []);
   if (loading) return <Loading />;
   return <div>Library</div>;
 }
